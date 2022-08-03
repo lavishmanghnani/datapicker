6d2f5c29c6c137d4546f07410e9607b3d3439f75
lenovo <lavish7939@gmail.com>
2022-08-03 17:30:20 +0530

    now added time and date picker updated

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
   File? image;
   List<File> images = [];
 
-  late DateTime selectedDateTime;
-
   bool pressed = false;
 
   Future imagePickerFromGallery() async {
@@ -70,10 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
       print('Failed to pick image : $e');
     }
   }
+  
 
   TimeOfDay selectedTime = TimeOfDay.now();
 
   TextEditingController dateInput = TextEditingController();
+  var date;
 
   @override
   void initState() {
@@ -101,21 +101,20 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: () async {
                     DateTime? pickedDate = await showDatePicker(
                         context: context,
-                        initialDate: DateTime.now(),
+                        initialDate:date ?? DateTime.now(),
                         firstDate: DateTime(1950),
                         //DateTime.now() - not to allow to choose before today.
                         lastDate: DateTime(2100));
 
                     if (pickedDate != null) {
-                      print(
-                          pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                      String formattedDate =
-                          DateFormat('yyyy-MM-dd').format(pickedDate);
-                      print(
-                          formattedDate); //formatted date output using intl package =>  2021-03-16
+                      print(pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                      String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+                      date = pickedDate;
+                      print(formattedDate);
+                      //formatted date output using intl package =>  2021-03-16
                       setState(() {
-                        dateInput.text =
-                            formattedDate; //set output date to TextField value.
+                        dateInput.text = formattedDate;
+                        //set output date to TextField value.
                       });
                     } else {}
                   },
